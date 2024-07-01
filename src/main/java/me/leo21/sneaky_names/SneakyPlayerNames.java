2a0f25de528c219e8ce15d48114b0c53f7d82049
Léo-21 <leo.kosman@proton.me>
2024-07-01 11:27:48 +0200

    add mod config file

--- src/main/java/me/leo21/sneaky_names/SneakyPlayerNames.java
+++ src/main/java/me/leo21/sneaky_names/SneakyPlayerNames.java
@@ -1,5 +1,6 @@
 package me.leo21.sneaky_names;
 
+import me.leo21.sneaky_names.config.ModConfig;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -7,7 +8,9 @@ import org.slf4j.LoggerFactory;
 
 public class SneakyPlayerNames implements ModInitializer {
 	public static final String MOD_ID = "sneaky_player_names";
+	public static final String MOD_NAME = "Sneaky Player Names";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static ModConfig CONFIG = new ModConfig();
 
 	@Override
 	public void onInitialize() {
